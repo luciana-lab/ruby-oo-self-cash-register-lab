7b245f6fc5631c6d7921e7ffb73a5dd5fef076de
Luciana Sugeta <lulusugeta@gmail.com>
2021-03-11 17:02:03 -0500

    commit tree f23eb615de7cdfad57c22b82e6d0f19473931c69

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,51 @@
+require 'pry'
+class CashRegister
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = items
+    end
 
+    def total=(total)
+        @total = total
+    end
+
+    def total
+        @total
+    end
+
+    def discount
+        @discount
+    end
+
+    def items
+        @items
+    end
+
+    def add_item(title, price, quantity = 1)
+        quantity.times do
+            @items << title
+        end
+        @last_total = @total
+        @total += price * quantity
+    end
+
+    def apply_discount
+        @total -= @total * @discount / 100
+        
+        if discount == 0
+        "There is no discount to apply."
+        else
+        "After the discount, the total comes to $#{@total}."
+        end
+    end
+
+    def items
+        @items.to_a
+    end
+
+    def void_last_transaction
+        @total = @last_total
+
+    end
+end
